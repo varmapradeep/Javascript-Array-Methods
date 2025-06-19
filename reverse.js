c655f09a3a14e30b43ed5c49b8e143c48a81f4d6
varma <varmapradeepvp@gmail.com>
2025-06-19 18:02:41 +0530

    Added reverse.js

---
+++ reverse.js
@@ -0,0 +1,16 @@
+//Syntax
+// array.reverse();     
+
+//Example
+
+let temparray = [22, 33, 11];
+let tempstring = ['Mango', 'Apple', 'Orange'];
+
+temparray.reverse();
+console.log(temparray);
+//Output: [ 11 ,33 , 22]
+
+tempstring.reverse();
+console.log(tempstring)
+//Output: [ 'Orange','Apple','Mango']
+
