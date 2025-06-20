21cb62e322f2dfad849eba775265b1e93c9e10b4
varma <varmapradeepvp@gmail.com>
2025-06-20 17:12:31 +0530

    Added isArray

---
+++ isArray.js
@@ -0,0 +1,17 @@
+//Syntax
+Array.isarray(obj);     
+
+//Example
+
+let tempnumber = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let tempstring = "KeralaVarma";
+
+console.log(Array.isArray(tempnumber));
+//Output: true
+
+console.log(Array.isArray(tempstring));
+//Output: false
+
+
+
+
